d0a8bb53600c546a72597ede4ca67163ec7142d9
Bonnie <bonniec15@nycstudents.net>
2022-10-09 11:00:16 -0400

    Parrot Class

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -13,6 +13,6 @@ public class LabRunner {
 //        System.out.println("Dear Jen, \nYou got an \"A\"! \n\nSincerely, \nThe Teacher");
 //        System.out.println(" * *\n* * *\n * *");
 //        System.out.println("\"Escape sequences are interesting\" \n _|___|_  \\(`^`\\)");
-        System.out.println("  /\\ \n /\"\"\\ \n\"  \"\\ \n \\\"  \"/");
+        System.out.println("   /\\ \n  /\"\"\\ \n /\"  \"\\ \n \\\"  \"/ \n  \\\"\"/ \n   \\/ ");
     }
 }
