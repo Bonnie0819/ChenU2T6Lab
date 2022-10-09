8b2cad61b81509b1de3b4dcda3603652cc9688d5
Bonnie <bonniec15@nycstudents.net>
2022-10-09 12:22:35 -0400

    Parrot Class

---
+++ src/StudentRunner.java
@@ -0,0 +1,16 @@
+public class StudentRunner {
+    public static void main (String [] args){
+//        Student student = new Student("Abby", "Smith", 2023);
+//        student.addTestScore(95);
+//        student.addTestScore(98);
+//        System.out.println(student.getStudentInfo());
+        int one = 1;
+
+        int two = 2;
+
+        String zee = "Z";
+
+        System.out.println(one + two + zee);
+
+    }
+}
